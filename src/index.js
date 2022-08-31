7a300158a54797d01c735039b305cb8257aef275
Raga <ragawilliam570@gmail.com>
2022-08-31 16:40:00 +0300

    Fixed simple linter errors on my end and finished on displaying number of likes on each element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // import { getLikes } from './modules/involvement.js';
+// import { getLikes } from './modules/involvement.js';
 import { loadDefault } from './modules/mainApi.js';
 import './style.css';
 
 loadDefault();
+// getLikes();
 // const loadHomepage = async () => {
 //   const likes = await getLikes();
 //   // const id = likes.item_id;
