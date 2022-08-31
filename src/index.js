bb90260dfcbd308b5b0f9f6ab4696afbddc4bd9d
Raga <ragawilliam570@gmail.com>
2022-08-31 15:16:41 +0300

    Made changes to index and landing page.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+// import { getLikes } from './modules/involvement.js';
 import { loadDefault } from './modules/mainApi.js';
 import './style.css';
 
 loadDefault();
+// const loadHomepage = async () => {
+//   const likes = await getLikes();
+//   // const id = likes.item_id;
+//   loadDefault(likes);
+// };
+
+// loadHomepage();
