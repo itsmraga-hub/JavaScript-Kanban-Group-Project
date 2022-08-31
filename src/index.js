d27bdc708075f58899534e1bccf5d8a9085d2a0a
ecekpo <ememekpo@yahoo.com>
2022-08-31 15:59:57 +0100

    Cleaned up the code on index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-// import { getLikes } from './modules/involvement.js';
-// import { getLikes } from './modules/involvement.js';
 import { loadDefault } from './modules/mainApi.js';
 import './style.css';
 
 loadDefault();
-// getLikes();
-// const loadHomepage = async () => {
-//   const likes = await getLikes();
-//   // const id = likes.item_id;
-//   loadDefault(likes);
-// };
-
-// loadHomepage();
