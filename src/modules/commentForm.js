dd3810147a5a56af275e6f6c877f0c2044638381
ecekpo <ememekpo@yahoo.com>
2022-08-31 15:53:28 +0100

    Selected the form and validated it making sure the data was filled

--- src/modules/commentForm.js
+++ src/modules/commentForm.js
@@ -1,19 +1,19 @@
-// import { comment } from './involvement.js';
+import { comment } from './involvement.js';
 
-// const formData = (form) => {
-//   if (form[0].value && form[1].value) {
-//     const name = form[0].value;
-//     const message = form[1].value;
-//     const id = form.getAttribute('data-id');
-//     const obj = {
-//       item_id: id,
-//       username: name,
-//       comment: message,
-//     };
-//     comment(obj);
-//     form[0].value = '';
-//     form[1].value = '';
-//   }
-// };
+const formData = (form) => {
+  if (form[0].value && form[1].value) {
+    const name = form[0].value;
+    const message = form[1].value;
+    const id = form.getAttribute('data-id');
+    const obj = {
+      item_id: id,
+      username: name,
+      comment: message,
+    };
+    comment(obj);
+    form[0].value = '';
+    form[1].value = '';
+  }
+};
 
 // export default formData;
