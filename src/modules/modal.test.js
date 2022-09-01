91812d8b35c6d4a1ad0f9df5f9915e1e8e82a48d
ecekpo <ememekpo@yahoo.com>
2022-09-01 13:32:14 +0100

    Wrote test for comment counter for each item popped up

--- src/modules/modal.test.js
+++ src/modules/modal.test.js
@@ -1,25 +1,25 @@
-// import commentsAPI from '../../__mocks__/comments.js';
-// import { commentCounter } from './modal.js';
+import commentsAPI from '../../__mocks__/comments.js';
+import { commentCounter } from './modal.js';
 
-// const commentsArr = commentsAPI;
-// /**
-//  * @jest-environment jsdom
-//  */
+const commentsArr = commentsAPI;
+/**
+ * @jest-environment jsdom
+ */
 
-// describe('Test - Count number of comments found in a particular item and make sure the number is correct', () => {
-//   test('Item 1 tests for its number of comments', () => {
-//     expect(commentCounter(commentsArr[0].comments)).toBe(1);
-//   });
-//   test('Item 2 tests for its number of comments', () => {
-//     expect(commentCounter(commentsArr[1].comments)).toBe(3);
-//   });
-//   test('Item 1 tests for its number of comments', () => {
-//     expect(commentCounter(commentsArr[2].comments)).toBe(3);
-//   });
-//   test('Item 1 tests for its number of comments', () => {
-//     expect(commentCounter(commentsArr[3].comments)).toBe(2);
-//   });
-//   test('Item 1 tests for its number of comments', () => {
-//     expect(commentCounter(commentsArr[4].comments)).toBe(0);
-//   });
-// });
+describe('Test - Count number of comments found in a particular item and make sure the number is correct', () => {
+  test('Item 1 tests for its number of comments', () => {
+    expect(commentCounter(commentsArr[0].comments)).toBe(1);
+  });
+  test('Item 2 tests for its number of comments', () => {
+    expect(commentCounter(commentsArr[1].comments)).toBe(3);
+  });
+  test('Item 1 tests for its number of comments', () => {
+    expect(commentCounter(commentsArr[2].comments)).toBe(3);
+  });
+  test('Item 1 tests for its number of comments', () => {
+    expect(commentCounter(commentsArr[3].comments)).toBe(2);
+  });
+  test('Item 1 tests for its number of comments', () => {
+    expect(commentCounter(commentsArr[4].comments)).toBe(0);
+  });
+});
